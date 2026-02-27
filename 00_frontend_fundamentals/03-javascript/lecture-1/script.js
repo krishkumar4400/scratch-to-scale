35d5e2ddaa5c910fb695a256434076f56f4f3023
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-28 05:08:15 +0530

    javascript introduction

--- 00_frontend_fundamentals/03-javascript/lecture-1/script.js
+++ 00_frontend_fundamentals/03-javascript/lecture-1/script.js
@@ -7,21 +7,20 @@
  * var let const 
  * console .... prompt alert
  * strings
- * string methods-template strings(literal) split join replace includes
- * 
- * 
- * 
- * 
+ * string methods-template strings(literal) slice split join replace includes
+ *
 
 js is a scripting language
-computers understands binary(0,1) 
+computers understands binary(0,1)
+
+
 
 krish -> interpreter/compiler -> 0101001010101
 
 js creator -> brendan eich works in netscape. build js language in 15 days bacause it build in few time so many issues are there with js 
 itni jldi bani to kaafi issues reh gye 
 jaldi baaji ka kaam ho hya
-errors and bugs choot gaya
+errors and bugs choot gaye
 to unhe fix karte karte js ke bahut saare versions aaye
 unme se ek sabse bada version aaya jiska naam tha es6 
 uske baad aur bhi version aaya par es6 sabase bada change raha js ki history me, kyuki js lagbhag completely  badal gayi 
@@ -34,6 +33,9 @@ matlab ki js ke issues and bugs and problems ko fix karne ke liye bahut sare nay
 // jab aapki html file chlegi tab hi aapka js bhi chal jaayega
 
 
+
+    kodr
+        - 
 
 
  */
@@ -52,3 +54,30 @@ console.log(alert);
 // console.log(alert());
 console.error("this is an error");
 console.warn("This is a warning");
+
+str = "Hey my name is krish kumar how are you"
+
+// splitting string into array
+console.log(str.split()) //['Hey my name is krish kumar how are you']
+console.log(str.split(' '));// ['Hey', 'my', 'name', 'is', 'krish', 'kumar', 'how', 'are', 'you']
+console.log(str.split('a'))// ['Hey my n', 'me is krish kum', 'r how ', 're you']
+
+// slicing
+console.log(str.slice(4,11)) // my name
+
+// split and join
+console.log(str.split(' ').join(' ')) // Hey my name is krish kumar how are you
+console.log(str.split(' ').join('a'))// Heyamyanameaisakrishakumarahowaareayou
+console.log(str.split('a').join(''))// Hey my nme is krish kumr how re you
+
+// replace the string 
+console.log(str.replace("a", "krish")) // Hey my nkrishme is krish kumar how are you
+console.log(str.replaceAll("a", "krishna")) // Hey my nkrishname is krish kumkrishnar how krishnare you
+
+// includes -> true || false
+console.log(str.includes("a")) // true
+console.log(str.includes("krish"))// true
+console.log(str.includes("krishna"))// false
+console.log(str.includes("kri"))// true
+console.log(str.includes(""))// true
+console.log(str.includes("werf"))// false
