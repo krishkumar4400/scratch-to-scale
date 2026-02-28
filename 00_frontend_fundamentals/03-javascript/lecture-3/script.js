88f66799ee2f16c7dc1fa88d470f00ca9d9bc8a3
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-28 07:26:35 +0530

    datatypes in javascript

---
+++ 00_frontend_fundamentals/03-javascript/lecture-3/script.js
@@ -0,0 +1,17 @@
+/**
+ * add comments
+ * float
+ * number
+ * string
+ * boolean
+ * null
+ * array
+ * object
+ * symbol - will learn when we will see objects in detail
+ * undefined 
+ * NaN
+ * infinity
+ * relative and primitive datatype in js
+ * 
+ * 
+ */
