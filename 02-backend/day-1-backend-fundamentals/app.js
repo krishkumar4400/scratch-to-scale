1a87f6aa521e2b23d034af4724ec1ce93c4f181b
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-03-01 22:31:28 +0530

    backend day-1 complete

--- 02-backend/day-1-backend-fundamentals/app.js
+++ 02-backend/day-1-backend-fundamentals/app.js
@@ -1,3 +1,12 @@
+// const catMe = require('cat-me');
+
+// console.log(catMe());
+
+
+// create server with express
 const express = require('express');
 
 const app = express(); // instane of express server
+
+
+app.listen(3000) // starts the server
