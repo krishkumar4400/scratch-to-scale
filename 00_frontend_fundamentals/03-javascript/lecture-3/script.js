9260d8eea7bd9a18bbebcb358ce4044ccf2a4a57
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-03-06 20:23:38 +0530

    Add datatypes in javascript

--- 00_frontend_fundamentals/03-javascript/lecture-3/script.js
+++ 00_frontend_fundamentals/03-javascript/lecture-3/script.js
@@ -1,17 +1,21 @@
 /**
  * add comments
- * float
- * number
- * string
- * boolean
- * null
- * array
- * object
+ * Datatypes - data ka type hi hai datatype
+ * float - 12.3
+ * number - 12
+ * string - "werfgvfdsf3"
+ * boolean - true/false
+ * null - jab aapke paas koi value na ho dene ke liye tab null use hota hai
+ * array - [] ek se jyada value hold karne ke liye
+ * object - {} ek se jyada properties kisi ek chiz ki hold karne ke liye
  * symbol - will learn when we will see objects in detail
- * undefined 
- * NaN
- * infinity
+ * undefined - jab koi variable banaya but usko koi value nahi di
+ * NaN - koi number ke sath aisa operation kiya ki ho nahi sakta to NaN milta hai.
+ * infinity - Infinity
  * relative and primitive datatype in js
  * 
+ * primitives - jinki value direct copy kar sakte hai like number, fload, string
+ * refrence - jinki value direct copy nhi kr sakte. like array, object, function.
  * 
  */
+
