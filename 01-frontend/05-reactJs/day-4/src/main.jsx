28d39373341cecdba1d3994f95bcb4f65b0fccf8
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-07-01 13:45:45 +0530

    create react app using vite

---
+++ 01-frontend/05-reactJs/day-4/src/main.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(App());
+
+// or
+root.render(<App />);
