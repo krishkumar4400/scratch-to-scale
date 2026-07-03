7d2426e5a307ad3daf763d6d13beccba92884c0e
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-07-03 08:52:35 +0530

    Implement props drilling

---
+++ 01-frontend/05-reactJs/day-13/src/App.jsx
@@ -0,0 +1,15 @@
+// Prop drilling and fragments
+
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <>
+    <h1>App</h1>
+    <Card />
+    </>
+  )
+}
+
+export default App
